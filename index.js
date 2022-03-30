c9171c14a109456815fb69d9d3aaf324d15f9367
shubham girase <sgirase08@gmail.com>
2022-03-30 11:22:31 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const server = http.createServer(app);
+app.use(cors());
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  //checking a user connection
+  console.log("a user connected");
+
+  //recieve emited event
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log("listening started .....");
+});
